200597c2c47dd4e22bef0f6ff7f11f8cae63c501
Joliver Estampador <joliverestampador@gmail.com>
2025-10-08 06:55:09 +0800

    creating first sprite

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,10 +7,15 @@ class Game extends Phaser.Scene {
 
   preload() {
     this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+      // you can add startFrame or endFrame
+    });
   }
 
   create(data) {
-    this.add.image(400, 300, 'logo');
+    this.add.sprite(400, 300, 'hero-run-sheet', 5);
   }
 
   update(time, delta) {}

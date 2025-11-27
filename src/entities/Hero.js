bdd807380b286c3e80af1fb3d3b95a32f1ff7222
Joliver Estampador <joliverestampador@gmail.com>
2025-11-27 08:48:12 +0800

    Logic State Transitions
    -Created this.input object in the constructor to store and access the reusable inputs globally
    - Corrected the transition spelling
    Added onEnterState to give us more info about the current state in our console
    Added implementations into our movePredicates that returns boolean that will help us to know if action needs to be transitioned
    
    https://docs.google.com/document/d/1ngrYtEekKfKnmKfk59SL1IM7-0cfhFAcShb8aAf6p44/edit?tab=t.0#heading=h.19v7ja55dql4

--- src/entities/Hero.js
+++ src/entities/Hero.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-import { StateMachine } from 'javascript-state-machine';
+import StateMachine from 'javascript-state-machine';
 
 class Hero extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y) {
@@ -33,6 +33,7 @@ class Hero extends Phaser.GameObjects.Sprite {
     this.body.setDragX(750);
 
     this.keys = scene.cursorKeys;
+    this.input = {};
 
     this.setupMovement();
   }
@@ -40,13 +41,17 @@ class Hero extends Phaser.GameObjects.Sprite {
   setupMovement() {
     this.moveState = new StateMachine({
       init: 'standing',
-      transistions: [
+      transitions: [
         { name: 'jump', from: 'standing', to: 'jumping'},
         { name: 'flip', from: 'jumping', to: 'flipping'},
         { name: 'fall', from: 'standing', to: 'falling'},
         { name: 'touchdown', from: ['jumping', 'flipping', 'falling'], to: 'standing'}, // from can be set to * (wildcard)
       ],
       methods: {
+        // give us info about state change and transition
+        onEnterState: (lifecycle) => {
+          console.log(lifecycle)
+        },
         // there are lifecycle like onEnterJumping()
         onJump: () => {
           this.body.setVelocityY(-400);
@@ -60,15 +65,26 @@ class Hero extends Phaser.GameObjects.Sprite {
     this.movePredicates = {
       // each of these will return boolean 
       // whether we should make the matching transition
-      jump: () => {},
-      flip: () => {},
-      fall: () => {},
-      touchdown: () => {},
+      jump: () => {
+        return this.input.didPressJump;
+      },
+      flip: () => {
+        return this.input.didPressJump;
+      },
+      fall: () => {
+        return !this.body.onFloor();
+      },
+      touchdown: () => {
+        return this.body.onFloor();
+      },
     }
   }
 
   preUpdate(time, delta) {
     super.preUpdate(time, delta);
+
+    this.input.didPressJump = Phaser.Input.Keyboard.JustDown(this.keys.up);
+
     if (this.keys.left.isDown) {
       // move character to the left
       this.body.setAccelerationX(-1000);
@@ -88,45 +104,16 @@ class Hero extends Phaser.GameObjects.Sprite {
       this.body.setAccelerationX(0);
     }
 
-    if (this.body.onFloor()) {
-      this.canDoubleJump = false;
-    }
-
-    if (this.body.velocity.y > 0) {
-      this.isJumping = false;
-    }
-
-    // tell us whether the key has just been pressed down or not.
-    // note about this method is that it will only return true the first time that you call
-    // if you want to use the the result of the test multiple times within a single frame you really need
-    // to assign it to a variable like we're doing here.
-    const didPressJump = Phaser.Input.Keyboard.JustDown(this.keys.up);
-
-    // jump logic, it will automatically pulled down
-    // onFloor() - make sure that it will jump if body is on the floor
-    if (didPressJump) {
-      // normal jump
-      if (this.body.onFloor()) {
-        this.isJumping = true;
-        this.canDoubleJump = true;
-        this.body.setVelocityY(-400);
-      } else if (this.canDoubleJump) {
-        this.isJumping = true;
-        this.body.setVelocityY(-300);
-
-        // reset to prevent continually bounce through the air
-        this.canDoubleJump = false;
-      }
-    }
-
     // if you hold the up button, it will jump full -400
     // if you just tap the up button, it will limit the jump to 150
-    if (!this.keys.up.isDown && this.body.velocity.y < -150 && this.isJumping) {
-      this.body.setVelocityY(-150);
+    if (this.moveState.is('jumping') || this.moveState.is('flipping')) {
+      if (!this.keys.up.isDown && this.body.velocity.y < -150) {
+        this.body.setVelocityY(-150);
+      }      
     }
 
     // code that calls transitions, check what transitions are valid
-    for (const t of this.moveState.transistions()) {
+    for (const t of this.moveState.transitions()) {
       if (t in this.movePredicates && this.movePredicates[t]()) {
         this.moveState[t]();
         break;
